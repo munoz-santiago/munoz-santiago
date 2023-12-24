58bfd58df9a9127ba4048a4ad45bf28288e8a328
Santiago  Muñoz <smunoz@Santiagos-MacBook-Pro.local>
2023-12-24 10:00:48 -0500

    Fix: Running docker container in droplet script

--- scripts/pull_and_run_docker_container.sh
+++ scripts/pull_and_run_docker_container.sh
@@ -8,11 +8,11 @@ docker pull $DOCKER_IMAGE
 docker stop smunoz-santiago-frontend-app || true
 docker rm -f smunoz-santiago-frontend-app || true
 
-docker rmi ghcr.io/munoz-santiago/munoz-santiago-repo/munoz-santiago-frontend-app || true
-docker pull ghcr.io/munoz-santiago/munoz-santiago-repo/munoz-santiago-frontend-app
+docker rmi ghcr.io/munoz-santiago/munoz-santiago/munoz-santiago-frontend-app || true
+docker pull ghcr.io/munoz-santiago/munoz-santiago/munoz-santiago-frontend-app
 
 # Run the Docker container on the droplet
 docker run -d --name smunoz-santiago-frontend-app -p 9001:80 \
     --network traefik_proxy \
     --label "traefik.http.routers.smunoz-santiago-frontend-app.rule=PathPrefix(\`/\`)" \
-    ghcr.io/munoz-santiago/munoz-santiago-repo/munoz-santiago-frontend-app
+    ghcr.io/munoz-santiago/munoz-santiago/munoz-santiago-frontend-app
